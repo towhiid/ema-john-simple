e6e08600e34e6d06ec979a3a5ae90ec77b853162
towhiid <towhid3008@gmail.com>
2020-08-26 21:31:06 +0600

    passed product from shop

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import fakeData from '../../fakeData';
+import { useState } from 'react';
+import './Shop.css';
+import Product from '../Product/Product';
+
+const Shop = () => {
+    const first10 = fakeData.slice(0, 10);
+    const [products, setProducts] = useState(first10);
+    return (
+        <div className="shop-container">
+            <div className="product-container">
+            {
+                products.map(items => <Product product = {items}></Product>)
+            }
+            
+            </div>
+            <div className="cart-container">
+                <h3>This is a cart</h3>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
